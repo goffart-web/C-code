611c4b4b594a8021fe146aad39352852156f46f1
goffart jean marc <kithulovalibin@gmail.com>
2024-10-20 14:18:20 +0300

    Update main.c

--- conditions/main.c
+++ conditions/main.c
@@ -2,7 +2,16 @@
 #include <stdlib.h>
 
 int main()
+// calculation of variables
 {
-    printf("Hello world!\n");
+    int num1=10, num2=2;
+    int mul= num1*num2;
+    int sub= num1-num2;
+    int add= num1+ num2;
+    int div= num1/num2;
+    printf("mul: %d\n",mul);
+    printf("sub: %d\n",sub);
+    printf("add: %d\n",add);
+    printf("div: %d\n",div);
     return 0;
 }
