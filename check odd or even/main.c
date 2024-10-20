e54b0b71efd5d1019cf99182da6f7350871be4a1
goffart jean marc <kithulovalibin@gmail.com>
2024-10-20 14:18:41 +0300

    Create main.c

---
+++ check odd or even/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num=0;
+    printf("enter a number :");
+    scanf("%d",&num);
+    if(num !odd);
+    printf(" this is not an odd number\n");
+    return 0;
+}
