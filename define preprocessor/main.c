154dda796fbc4283478576ed3114747ca8ab2a52
goffart jean marc <kithulovalibin@gmail.com>
2024-10-23 15:21:40 +0300

    Create main.c

---
+++ define preprocessor/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define PI 3,14
+
+void main()
+{   int r,area;
+
+    printf("please enter the radius of circle\n");
+    scanf("%d",&r);
+    area = PI *(r*r);
+    printf("area of the circle=%d,area");
+
+}
