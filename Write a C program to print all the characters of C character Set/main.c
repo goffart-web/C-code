dbc86c77b64ed9f646f208bf031851fbd69a9993
goffart jean marc <kithulovalibin@gmail.com>
2024-10-23 15:30:04 +0300

    Create main.c

---
+++ Write a C program to print all the characters of C character Set/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+ // Print all printable ASCII characters printf("Printable ASCII characters:\n");
+ for (int i =32; i <=126; i++) {
+ printf("%c ", (char)i);
+ }
+ printf("\n");
+
+  return 0;
+}
+
+
