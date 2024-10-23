5e94d68cca5beadff98d26a94ecae6233bb742ad
goffart jean marc <kithulovalibin@gmail.com>
2024-10-23 15:27:28 +0300

    Create main.c

---
+++ type casting/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    int a,b,c;
+    float avg ;
+    printf("Enter any three integer values :") ;
+    scanf("%d%d%d",&a, &b, &c) ;
+
+    avg = (a +b +c )/3 ;
+    printf("avg before casting =%f\n",avg);
+
+    avg = (float)( a+ b+c)/3 ;
+    printf(" avg after casting =%f\n",avg);
+    return 0;
+}
+
+
+
+
