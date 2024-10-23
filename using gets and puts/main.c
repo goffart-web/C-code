661f6ee0e9ce1432828221d60648626d02d0ef81
goffart jean marc <kithulovalibin@gmail.com>
2024-10-23 15:29:00 +0300

    Create main.c

---
+++ using gets and puts/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+     /* character array of length 100 */
+    char str[100];
+    printf("Enter a string");
+    gets( str );
+    puts( str );
+    getch();
+    return 0;
+}
