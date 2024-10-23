76b6545159fe1562794d47c0a7805158b6432659
goffart jean marc <kithulovalibin@gmail.com>
2024-10-23 15:23:13 +0300

    Create main.c

---
+++ Divisible by 5/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+       int n ;
+    	printf("Enter any integer number: ") ;
+    	scanf("%d", &n) ;
+   if ( n%5 == 0 )
+   	printf("Given number is divisible by 5\n") ;
+   else	printf("statement does not belong to if!!!") ;
+
+    return 0;
+}
