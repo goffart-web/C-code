bb0a8ac84e98258b5540cd287516fcda2ce007d7
goffart jean marc <kithulovalibin@gmail.com>
2024-10-23 15:29:10 +0300

    Create main.c

---
+++ using printf and scanf/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+        // defining a variable
+    int i;
+//displaying message on the screen asking the user to input a value
+    printf("Please enter a value...");
+    // reading the value entered by the user
+    scanf("%d", &i);
+    // displaying the number as output
+    printf( "\n You entered: %d", i);
+
+    return 0;
+}
