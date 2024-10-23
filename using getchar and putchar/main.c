71a3cb4be4f37ecc3d5f75c1aafbb43775f6b49d
goffart jean marc <kithulovalibin@gmail.com>
2024-10-23 15:28:39 +0300

    Create main.c

---
+++ using getchar and putchar/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+       	printf("Enter a character");
+// Take a character as input & store it in variable c
+	c = getchar();
+// display the character stored in variable c
+	putchar(c);
+
+    return 0;
+}
