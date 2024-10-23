620f39fce6b0d5e41348deae392c0f5d1c585e32
goffart jean marc <kithulovalibin@gmail.com>
2024-10-23 15:21:36 +0300

    Create main.c

---
+++ 32keywords/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    printf("int\n");
+    printf("float\n");
+    printf("double\n");
+    printf("char\n");
+    printf("void\n");
+    printf("break\n");
+    printf("case\n");
+    printf("swicht\n");
+    printf("if\n");
+    printf("else\n");
+    printf("else if\n");
+    printf("continue\n");
+    printf("goto\n");
+    printf("for\n");
+    printf("while\n");
+    printf("do\n");
+    printf("auto\n");
+    printf("default\n");
+    printf("return\n");
+    printf("long\n");
+    printf("short\n");
+    printf("const\n");
+    printf("enum\n");
+    printf("sizeof\n");
+    printf("static\n");
+    printf("struct\n");
+    printf("union\n");
+    printf("signed\n");
+    printf("unsigned\n");
+    printf("volatile\n");
+    printf("extern\n");
+    printf("regester\n");
+    return 0;
+}
