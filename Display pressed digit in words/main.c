eddb96adbba827a740366ee654f0ed0db3d0c187
goffart jean marc <kithulovalibin@gmail.com>
2024-10-23 15:22:34 +0300

    Create main.c

---
+++ Display pressed digit in words/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{int n ;
+    printf("Enter any digit: ") ;
+   scanf("%d", &n) ;
+   switch( n )
+   {
+   	case 0: printf("ZERO") ;
+        	break ;
+   	case 1: printf("ONE") ;
+        	break ;
+   	case 2: printf("TWO") ;
+        	break ;
+case 3: printf("THREE") ;
+        	break ;
+        	int n ;
+    printf("Enter any digit: ") ;
+   scanf("%d", &n) ;
+        	break ;
+        	case 4: printf("FOUR") ;
+		break ;
+   	case 5: printf("FIVE") ;
+        	break ;
+   	case 6: printf("SIX") ;
+        	break ;
+   	case 7: printf("SEVEN") ;
+        	break ;
+   	case 8: printf("EIGHT") ;
+        	break ;
+   	case 9: printf("NINE") ;
+        	break ;
+   	default: printf("Not a Digit") ;
+    return 0;}
+}
